4961c21aeb57cc0f9d8eb3e0e3cd69c7c4ed1eac
Matias Lago Evia <matiaslagoevia@gmail.com>
2025-10-02 08:31:50 -0400

    Decouple from personal laptop directory structure

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 docker run -it \
     --rm \
     --name dbox \
-    -v ~/code/devbox/.config:/root/.config/ \
+    -v ./.config:/root/.config/ \
     -v ~/.gitconfig:/root/.gitconfig:ro \
     -v ~/.ssh:/root/.ssh:ro \
     mle/dbox:latest
