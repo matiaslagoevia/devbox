4469617ba32323f3b58eb6964138af9aee6cf635
Matias Lago Evia <matiaslagoevia@gmail.com>
2025-10-02 08:18:23 -0400

    Forward host git & ssh state

--- run.sh
+++ run.sh
@@ -4,4 +4,6 @@ docker run -it \
     --rm \
     --name dbox \
     -v ~/code/devbox/.config:/root/.config/ \
+    -v ~/.gitconfig:/root/.gitconfig:ro \
+    -v ~/.ssh:/root/.ssh:ro \
     mle/dbox:latest
