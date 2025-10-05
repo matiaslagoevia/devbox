8807d74fc45c13e8bd09fe4a1478ef0f9cd0e9d4
Matias Lago Evia <matiaslagoevia@gmail.com>
2025-10-04 21:57:35 -0400

    Fix compatibility again

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 docker run -it \
     --rm \
     --name dbox \
+    --platform linux/arm64 \
     -v ./.config:/root/.config/ \
     -v ./.tools:/root/.tools/ \
     -v ~/.gitconfig:/root/.gitconfig:ro \
