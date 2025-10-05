23034650187e6c4691b5168e0e6c7c757e741a88
Matias Lago Evia <matiaslagoevia@gmail.com>
2025-10-04 20:54:45 -0400

    Add line-number toggle for copy-pasting

--- .config/nvim/lua/config/keybinds.lua
+++ .config/nvim/lua/config/keybinds.lua
@@ -15,3 +15,19 @@ map("n", "<leader>l", "<C-W><C-L>", { silent = true })
 map("n", "<leader>h", "<C-W><C-H>", { silent = true })
 map("n", "<leader>k", "<C-W><C-K>", { silent = true })
 map("n", "<leader>j", "<C-W><C-J>", { silent = true })
+
+-- Toggle line numbers (useful to be able to mouse-copy text from iTerm2)
+vim.keymap.set("n", "<leader>ln", function()
+  if vim.wo.number or vim.wo.relativenumber then
+    vim.wo.number = false
+    vim.wo.relativenumber = false
+    vim.wo.signcolumn = "no"
+    print("Line numbers and signs hidden")
+  else
+    vim.wo.number = true
+    vim.wo.relativenumber = true
+    vim.wo.signcolumn = "auto"
+    print("Line numbers and signs restored")
+  end
+end)
+
